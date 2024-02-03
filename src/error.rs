fa9cc99fc735b44a1244cb5e185d9ba28c5662ca
bouzuya <m@bouzuya.net>
2024-02-03 09:26:26 +0900

    Add doc for error mod

--- src/error.rs
+++ src/error.rs
@@ -1,7 +1,10 @@
+/// A result type for this crate.
 pub type Result<T> = std::result::Result<T, Error>;
 
+/// An error that can occur when working with this crate.
 #[derive(Debug, thiserror::Error)]
 pub enum Error {
+    /// [`serde_firestore_value::to_value`] error
     #[error("to value: {0}")]
     ToValue(#[from] serde_firestore_value::Error),
 }
