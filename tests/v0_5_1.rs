466dbb5e77e55d6ef16e7bf55a5485768ef80f8c
bouzuya <m@bouzuya.net>
2024-02-03 10:07:33 +0900

    Fix tests for v0.5.1

--- tests/v0_5_1.rs
+++ tests/v0_5_1.rs
@@ -5,5 +5,10 @@ fn test_error() -> firestore_structured_query::Result<()> {
     fn assert_impl<T: std::error::Error + Send + Sync>(_: T) {}
     let e: Error = to_value(&1_u64).unwrap_err();
     assert_impl(e);
+    match to_value(&1_u64).unwrap_err() {
+        Error::ToValue(e) => {
+            assert_eq!(e.to_string(), "u64 is not supported");
+        }
+    }
     Ok(())
 }
