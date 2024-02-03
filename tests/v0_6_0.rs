aad539ac13c5e44dd287410c534abe7fc56a1c49
bouzuya <m@bouzuya.net>
2024-02-03 12:18:58 +0900

    Sort tests

--- tests/v0_6_0.rs
+++ tests/v0_6_0.rs
@@ -195,6 +195,101 @@ fn test_field_path_in() -> firestore_structured_query::Result<()> {
 }
 
 #[test]
+fn test_field_path_is_nan() -> firestore_structured_query::Result<()> {
+    // Added: FieldPath::is_nan
+    use firestore_structured_query::FieldPath;
+    use google_api_proto::google::firestore::v1::structured_query;
+    let filter1 = FieldPath::raw("field11").is_nan()?;
+    assert_eq!(
+        structured_query::Filter::from(filter1),
+        structured_query::Filter {
+            filter_type: Some(structured_query::filter::FilterType::UnaryFilter(
+                structured_query::UnaryFilter {
+                    op: structured_query::unary_filter::Operator::IsNan as i32,
+                    operand_type: Some(structured_query::unary_filter::OperandType::Field(
+                        structured_query::FieldReference {
+                            field_path: "field11".to_string()
+                        }
+                    )),
+                },
+            )),
+        }
+    );
+    Ok(())
+}
+
+#[test]
+fn test_field_path_is_not_nan() -> firestore_structured_query::Result<()> {
+    // Added: FieldPath::is_not_nan
+    use firestore_structured_query::FieldPath;
+    use google_api_proto::google::firestore::v1::structured_query;
+    let filter1 = FieldPath::raw("field12").is_not_nan()?;
+    assert_eq!(
+        structured_query::Filter::from(filter1),
+        structured_query::Filter {
+            filter_type: Some(structured_query::filter::FilterType::UnaryFilter(
+                structured_query::UnaryFilter {
+                    op: structured_query::unary_filter::Operator::IsNotNan as i32,
+                    operand_type: Some(structured_query::unary_filter::OperandType::Field(
+                        structured_query::FieldReference {
+                            field_path: "field12".to_string()
+                        }
+                    )),
+                },
+            )),
+        }
+    );
+    Ok(())
+}
+
+#[test]
+fn test_field_path_is_not_null() -> firestore_structured_query::Result<()> {
+    // Added: FieldPath::is_not_null
+    use firestore_structured_query::FieldPath;
+    use google_api_proto::google::firestore::v1::structured_query;
+    let filter1 = FieldPath::raw("field13").is_not_null()?;
+    assert_eq!(
+        structured_query::Filter::from(filter1),
+        structured_query::Filter {
+            filter_type: Some(structured_query::filter::FilterType::UnaryFilter(
+                structured_query::UnaryFilter {
+                    op: structured_query::unary_filter::Operator::IsNotNull as i32,
+                    operand_type: Some(structured_query::unary_filter::OperandType::Field(
+                        structured_query::FieldReference {
+                            field_path: "field13".to_string()
+                        }
+                    )),
+                },
+            )),
+        }
+    );
+    Ok(())
+}
+
+#[test]
+fn test_field_path_is_null() -> firestore_structured_query::Result<()> {
+    // Added: FieldPath::is_null
+    use firestore_structured_query::FieldPath;
+    use google_api_proto::google::firestore::v1::structured_query;
+    let filter1 = FieldPath::raw("field14").is_null()?;
+    assert_eq!(
+        structured_query::Filter::from(filter1),
+        structured_query::Filter {
+            filter_type: Some(structured_query::filter::FilterType::UnaryFilter(
+                structured_query::UnaryFilter {
+                    op: structured_query::unary_filter::Operator::IsNull as i32,
+                    operand_type: Some(structured_query::unary_filter::OperandType::Field(
+                        structured_query::FieldReference {
+                            field_path: "field14".to_string()
+                        }
+                    )),
+                },
+            )),
+        }
+    );
+    Ok(())
+}
+#[test]
 fn test_field_path_less_than() -> firestore_structured_query::Result<()> {
     // Added: FieldPath::less_than
     use firestore_structured_query::FieldPath;
@@ -293,104 +388,6 @@ fn test_field_path_not_in() -> firestore_structured_query::Result<()> {
                             }]
                         }))
                     })
-                },
-            )),
-        }
-    );
-    Ok(())
-}
-
-// unary filter
-
-#[test]
-fn test_field_path_is_nan() -> firestore_structured_query::Result<()> {
-    // Added: FieldPath::is_nan
-    use firestore_structured_query::FieldPath;
-    use google_api_proto::google::firestore::v1::structured_query;
-    let filter1 = FieldPath::raw("field11").is_nan()?;
-    assert_eq!(
-        structured_query::Filter::from(filter1),
-        structured_query::Filter {
-            filter_type: Some(structured_query::filter::FilterType::UnaryFilter(
-                structured_query::UnaryFilter {
-                    op: structured_query::unary_filter::Operator::IsNan as i32,
-                    operand_type: Some(structured_query::unary_filter::OperandType::Field(
-                        structured_query::FieldReference {
-                            field_path: "field11".to_string()
-                        }
-                    )),
-                },
-            )),
-        }
-    );
-    Ok(())
-}
-
-#[test]
-fn test_field_path_is_not_nan() -> firestore_structured_query::Result<()> {
-    // Added: FieldPath::is_not_nan
-    use firestore_structured_query::FieldPath;
-    use google_api_proto::google::firestore::v1::structured_query;
-    let filter1 = FieldPath::raw("field12").is_not_nan()?;
-    assert_eq!(
-        structured_query::Filter::from(filter1),
-        structured_query::Filter {
-            filter_type: Some(structured_query::filter::FilterType::UnaryFilter(
-                structured_query::UnaryFilter {
-                    op: structured_query::unary_filter::Operator::IsNotNan as i32,
-                    operand_type: Some(structured_query::unary_filter::OperandType::Field(
-                        structured_query::FieldReference {
-                            field_path: "field12".to_string()
-                        }
-                    )),
-                },
-            )),
-        }
-    );
-    Ok(())
-}
-
-#[test]
-fn test_field_path_is_not_null() -> firestore_structured_query::Result<()> {
-    // Added: FieldPath::is_not_null
-    use firestore_structured_query::FieldPath;
-    use google_api_proto::google::firestore::v1::structured_query;
-    let filter1 = FieldPath::raw("field13").is_not_null()?;
-    assert_eq!(
-        structured_query::Filter::from(filter1),
-        structured_query::Filter {
-            filter_type: Some(structured_query::filter::FilterType::UnaryFilter(
-                structured_query::UnaryFilter {
-                    op: structured_query::unary_filter::Operator::IsNotNull as i32,
-                    operand_type: Some(structured_query::unary_filter::OperandType::Field(
-                        structured_query::FieldReference {
-                            field_path: "field13".to_string()
-                        }
-                    )),
-                },
-            )),
-        }
-    );
-    Ok(())
-}
-
-#[test]
-fn test_field_path_is_null() -> firestore_structured_query::Result<()> {
-    // Added: FieldPath::is_null
-    use firestore_structured_query::FieldPath;
-    use google_api_proto::google::firestore::v1::structured_query;
-    let filter1 = FieldPath::raw("field14").is_null()?;
-    assert_eq!(
-        structured_query::Filter::from(filter1),
-        structured_query::Filter {
-            filter_type: Some(structured_query::filter::FilterType::UnaryFilter(
-                structured_query::UnaryFilter {
-                    op: structured_query::unary_filter::Operator::IsNull as i32,
-                    operand_type: Some(structured_query::unary_filter::OperandType::Field(
-                        structured_query::FieldReference {
-                            field_path: "field14".to_string()
-                        }
-                    )),
                 },
             )),
         }
