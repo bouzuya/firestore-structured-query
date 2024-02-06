97dbf96c2f77488a23b73044133e5513af8ded5e
bouzuya <m@bouzuya.net>
2024-02-07 06:28:05 +0900

    Add doc for FieldPath::equal

--- src/field_path.rs
+++ src/field_path.rs
@@ -184,6 +184,47 @@ impl FieldPath {
         )
     }
 
+    /// Creates a new `FieldFilter` with the `Equal` operator.
+    ///
+    /// <https://firebase.google.com/docs/firestore/reference/rpc/google.firestore.v1#google.firestore.v1.StructuredQuery.FieldFilter>
+    /// <https://firebase.google.com/docs/firestore/reference/rpc/google.firestore.v1#google.firestore.v1.StructuredQuery.FieldFilter.Operator.ENUM_VALUES.google.firestore.v1.StructuredQuery.FieldFilter.Operator.EQUAL>
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// # fn test_field_path_equal() -> firestore_structured_query::Result<()> {
+    /// use firestore_structured_query::{FieldPath, IntoValue, Result};
+    /// use google_api_proto::google::firestore::v1::{structured_query, value::ValueType, Value};
+    /// struct S(i64);
+    /// impl IntoValue for S {
+    ///     fn into_value(self) -> Result<Value> {
+    ///         Ok(Value {
+    ///             value_type: Some(ValueType::IntegerValue(self.0)),
+    ///         })
+    ///     }
+    /// }
+    /// let filter1 = FieldPath::raw("field5").equal(Value {
+    ///     value_type: Some(ValueType::IntegerValue(5)),
+    /// })?;
+    /// let filter2 = FieldPath::raw("field5").equal(S(5))?;
+    /// let expected = structured_query::Filter {
+    ///     filter_type: Some(structured_query::filter::FilterType::FieldFilter(
+    ///         structured_query::FieldFilter {
+    ///             field: Some(structured_query::FieldReference {
+    ///                 field_path: "field5".to_string(),
+    ///             }),
+    ///             op: structured_query::field_filter::Operator::Equal as i32,
+    ///             value: Some(Value {
+    ///                 value_type: Some(ValueType::IntegerValue(5)),
+    ///             }),
+    ///         },
+    ///     )),
+    /// };
+    /// assert_eq!(structured_query::Filter::from(filter1), expected);
+    /// assert_eq!(structured_query::Filter::from(filter2), expected);
+    /// #     Ok(())
+    /// # }
+    /// ```
     pub fn equal<T>(&self, value: T) -> Result<Filter>
     where
         T: IntoValue,
