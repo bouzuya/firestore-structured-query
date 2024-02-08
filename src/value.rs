00014c41366dabc005dbcf1ce15ebe84bc4131b4
bouzuya <m@bouzuya.net>
2024-02-08 22:49:56 +0900

    Add doc for to_value

--- src/value.rs
+++ src/value.rs
@@ -1,5 +1,17 @@
 use crate::Result;
 
+/// A Firestore value converter.
+///
+/// # Examples
+///
+/// ```rust
+/// # fn test_to_value() -> firestore_structured_query::Result<()> {
+/// use google_api_proto::google::firestore::v1::{Value, value::ValueType};
+/// use firestore_structured_query::to_value;
+/// assert_eq!(to_value(&1)?, Value { value_type: Some(ValueType::IntegerValue(1)) });
+/// #     Ok(())
+/// # }
+/// ```
 #[cfg(feature = "serde")]
 pub fn to_value<T>(v: &T) -> Result<google_api_proto::google::firestore::v1::Value>
 where
