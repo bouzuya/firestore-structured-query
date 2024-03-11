cc8186058b99e394f82ae9ed6803cd6d69f38018
bouzuya <m@bouzuya.net>
2024-03-11 23:44:59 +0900

    Fix FieldPath::new to support quoted field name

--- src/field_path.rs
+++ src/field_path.rs
@@ -50,43 +50,56 @@ impl FieldPath {
     /// use google_api_proto::google::firestore::v1::structured_query;
     ///
     /// // simple field name
-    /// let field_path1 = FieldPath::new(["field1"])?;
+    /// let field_path1 = FieldPath::new(["field1"]);
     /// assert_eq!(
     ///     structured_query::FieldReference::from(field_path1),
     ///     structured_query::FieldReference {
     ///         field_path: "field1".to_string(),
     ///     }
     /// );
-    /// let field_path2 = FieldPath::new(["field1", "field2"])?;
+    /// let field_path2 = FieldPath::new(["field1", "field2"]);
     /// assert_eq!(
     ///     structured_query::FieldReference::from(field_path2),
     ///     structured_query::FieldReference {
     ///         field_path: "field1.field2".to_string(),
     ///     }
     /// );
+    /// let field_path3 = FieldPath::new(["foo", "x&y"]);
+    /// assert_eq!(
+    ///     structured_query::FieldReference::from(field_path3),
+    ///     structured_query::FieldReference {
+    ///         field_path: "foo.`x&y`".to_string(),
+    ///     }
+    /// );
+    /// let field_path4 = FieldPath::new(["a`b", r#"a\b"#]);
+    /// assert_eq!(
+    ///     structured_query::FieldReference::from(field_path4),
+    ///     structured_query::FieldReference {
+    ///         field_path: r#"`a\`b`.`a\\b`"#.to_string(),
+    ///     }
+    /// );
     /// #     Ok(())
     /// # }
     /// ```
-    pub fn new<I>(field_names: I) -> Result<Self>
+    pub fn new<I>(field_names: I) -> Self
     where
         I: IntoIterator,
         I::Item: Into<String>,
     {
-        Ok(Self(
+        Self(
             field_names
                 .into_iter()
                 .map(Into::into)
                 .map(|s| {
                     if is_simple_field_name(&s) {
-                        Ok(s)
+                        s
                     } else {
-                        // FIXME: validate quoted field name
-                        Err(crate::Error::new(format!("invalid field name: {}", s)))
+                        format!("`{}`", s.replace('`', r#"\`"#))
                     }
                 })
-                .collect::<Result<Vec<String>>>()?
+                .collect::<Vec<String>>()
                 .join("."),
-        ))
+        )
     }
 
     /// Creates a new field path without escaping.
